39e6b0dba3958e4762e30013230dabdd8243b0ee
CasaNovack <92743291+CasaNovack@users.noreply.github.com>
2023-02-26 09:42:08 +0100

    Add files via upload

---
+++ Przychodnia/src/test/java/Testy.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+public class Testy {
+
+    @Test
+    public void main(){
+    }
+    @Test
+    public void getVisitByIdl() {
+    }
+    @Test
+    public void saveVisit() {
+    }
+    @Test
+    public void updateVisit() {
+    }
+}
